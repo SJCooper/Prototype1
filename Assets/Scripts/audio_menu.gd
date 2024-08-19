ba87790d31f4c53630891b2a54eda71c50c4500b
Simon 'Cooper' Coupe <Simonjosephcoupe@gmail.com>
2024-08-19 18:47:56 +0100

    removed audio after mute test working
    removed repetative audio file, audio manager scene and removed from auto load. mute is working for master bus in audio.

--- Assets/Scripts/audio_menu.gd
+++ Assets/Scripts/audio_menu.gd
@@ -2,11 +2,7 @@ extends Control
 
 
 func _on_mute_toggled(toggled_on):
-	AudioServer.is_bus_mute(0)
-
-
-
-
+	AudioServer.set_bus_mute(0,toggled_on)
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://Assets/Scenes/options_menu.tscn")
